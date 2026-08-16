6d02151db22b13e3bacb912c66a995d937aac930
Felicitas Ocampo <focampo@mail.austral.edu.ar>
2026-08-16 19:54:15 -0300

    add: agrego pluging a archivos de gradle de cli e interpreter

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("printscript.kotlin-common-conventions")
+}
+
 dependencies {
     implementation(project(":lexer"))
     implementation(project(":parser"))
--- interpreter/build.gradle.kts
+++ interpreter/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("printscript.kotlin-common-conventions")
+}
+
 dependencies {
     implementation(project(":ast"))
     implementation(project(":common"))
