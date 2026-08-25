3fa1041d4ace3121a78fe9097fabb16d561e255e
Felicitas Ocampo <focampo@mail.austral.edu.ar>
2026-08-25 01:00:35 -0300

    feature: printscript formatter

--- formatter/build.gradle.kts
+++ formatter/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 dependencies{
     implementation(project(":ast"))
     implementation(project(":common"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
 }
 
 tasks.test {
--- formatter/src/main/kotlin/FormatterRules.kt
+++ formatter/src/main/kotlin/FormatterRules.kt
@@ -1,2 +1,12 @@
-class FormatterRules {
+data class FormatterRules(
+    val spaceBeforeColon: Boolean = false,
+    val spaceAfterColon: Boolean = true,
+    val spaceAroundAssignment: Boolean = true,
+    val lineBreaksBeforePrintln: Int = 1,
+) {
+    init {
+        require(lineBreaksBeforePrintln in 0..2) {
+            "lineBreaksBeforePrintln must be 0, 1 or 2, was $lineBreaksBeforePrintln"
+        }
+    }
 }
