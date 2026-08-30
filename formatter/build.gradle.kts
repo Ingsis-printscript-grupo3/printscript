dd4c53d5609bb396eccdc8cdd837e96638c7aa3c
Felicitas Ocampo <focampo@mail.austral.edu.ar>
2026-08-30 11:36:39 -0300

    fix: muevo jacoco a gadle compartido, agrego detekt y .config

--- formatter/build.gradle.kts
+++ formatter/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.gradle.kotlin.dsl.jacoco
-
 plugins {
     id("printscript.kotlin-common-conventions")
-    jacoco
 }
 
 dependencies {
@@ -10,24 +7,4 @@ dependencies {
     implementation(project(":common"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-}
-
-tasks.jacocoTestCoverageVerification {
-    violationRules {
-        rule {
-            limit { minimum = "0.80".toBigDecimal() }
-        }
-    }
-}
-
-tasks.check {
-    dependsOn(tasks.jacocoTestCoverageVerification)
 }
--- interpreter/build.gradle.kts
+++ interpreter/build.gradle.kts
@@ -1,18 +1,8 @@
 plugins {
     id("printscript.kotlin-common-conventions")
-    jacoco
 }
 
 dependencies {
     implementation(project(":ast"))
     implementation(project(":common"))
-}
-
-// por ahora solo el reporte, sin enganchar el umbral a check
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
 }
