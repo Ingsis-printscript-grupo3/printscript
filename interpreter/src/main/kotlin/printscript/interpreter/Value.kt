b355f4f5bbd0f43798346d70c84c8481e1069f6a
Eliseo <ealanistoledo@mail.austral.edu.ar>
2026-09-07 15:56:52 -0300

    feat(interpreter): add BooleanValue and 1.1 runtime error classes

--- interpreter/src/main/kotlin/printscript/interpreter/Value.kt
+++ interpreter/src/main/kotlin/printscript/interpreter/Value.kt
@@ -6,14 +6,18 @@ data class NumberValue(val value: Double) : Value
 
 data class StringValue(val value: String) : Value
 
+data class BooleanValue(val value: Boolean) : Value
+
 fun Value.textOf(): String =
     when (this) {
         is NumberValue -> if (this.value % 1.0 == 0.0) this.value.toLong().toString() else this.value.toString()
         is StringValue -> this.value
+        is BooleanValue -> this.value.toString()
     }
 
 fun Value.typeName(): String =
     when (this) {
         is NumberValue -> "number"
         is StringValue -> "string"
+        is BooleanValue -> "boolean"
     }
